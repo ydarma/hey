bc339acdd8199e07b1576593dcbf8aa7ef55c702
YDarma <yves.darmaillac@arone.com>
2021-12-09 22:45:42 +0100

    ace editor integration

--- src/components/HeyEditor.vue
+++ src/components/HeyEditor.vue
@@ -1,11 +1,34 @@
 <template>
-  <h2>This is an Hey editor</h2>
+  <div>
+    <h2>This is an Hey editor</h2>
+    <div id="editor"></div>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, onMounted } from "vue";
+import ace from "ace-builds";
+import "ace-builds/webpack-resolver";
 
 export default defineComponent({
   name: "HeyEditor",
+  setup() {
+    let editor;
+    onMounted(() => {
+      editor = ace.edit("editor", {
+        maxLines: 50,
+        minLines: 50,
+        value: "var hello = 'world'" + "\n",
+        mode: "ace/mode/javascript",
+      });
+    });
+    return { editor };
+  },
 });
 </script>
+
+<style>
+#editor {
+  height: 700px;
+}
+</style>
