c1b492c76c496d2e8a02c3aa95ec928a6407651f
YDarma <yves.darmaillac@arone.com>
2021-12-12 17:41:52 +0100

    css

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,7 @@
     </div>
     <div class="p-2 w-50">
       <div class="d-flex flex-column h-100">
-        <HeyEditor class="mb-2" />
+        <HeyEditor class="mb-2 border rounded p-1 flex-fill editor" />
         <div>
           <b-button variant="danger" class="mx-3" pill @click="exec()">
             <b-icon size="lg" icon="play"></b-icon>
@@ -14,7 +14,7 @@
             <b-icon size="lg" icon="x"></b-icon>
           </b-button>
         </div>
-        <div class="p-2 flex-fill">
+        <div class="p-2 flex-fill result">
           <HeyOut />
         </div>
       </div>
@@ -41,3 +41,11 @@ export default defineComponent({
   },
 });
 </script>
+
+<style>
+.editor,
+.result {
+  min-height: 30%;
+  max-height: 45%;
+}
+</style>
