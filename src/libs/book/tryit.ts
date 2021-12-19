c6c75ede36856a38d51bb09fa958259d8f2c2916
YDarma <yves.darmaillac@arone.com>
2021-12-19 16:26:26 +0100

    Functions started

--- src/libs/book/tryit.ts
+++ src/libs/book/tryit.ts
@@ -4,10 +4,11 @@ export function tryit(loader: (source: string) => void): void {
   $(".language-hey")
     .parent()
     .each((_, e) => {
+      $(e).addClass("mx-5 my-3");
       $(e).append(
         $("<button/>")
           .text("Essayer")
-          .addClass("btn btn-outline-primary btn-sm m-2")
+          .addClass("btn btn-outline-primary btn-sm m-1")
           .on({
             click: () => {
               loader($(e).find("code").prop("innerText").trim());
