f66db2cfe665310a4ce782fe5cd9dc972ecfc5cc
YDarma <yves.darmaillac@arone.com>
2021-12-21 23:23:34 +0100

    license

--- src/App.vue
+++ src/App.vue
@@ -8,8 +8,8 @@
     Contribute on GitHub
   </a>
   <b-card class="fixed-top menu">
-    <div class="d-flex">
-      <div class="w-50">
+    <div class="d-flex justify-content-between align-items-center">
+      <div class="layout">
         <b-button-group>
           <b-button
             variant="success"
@@ -30,22 +30,37 @@
           </b-button>
         </b-button-group>
       </div>
-      <div class="w-50">
-        <b-button
-          class="m-1 d-none d-lg-inline py-2 px-3"
-          variant="success"
-          to="/"
-        >
+      <div class="toc mx-5 d-flex align-items-center">
+        <b-button class="m-1 d-none d-lg-inline px-3" variant="success" to="/">
           <b-icon size="lg" icon="house"></b-icon>
         </b-button>
         <table-of-content class="mx-1"></table-of-content>
         <b-button
-          class="m-1 d-none d-lg-inline py-2 px-3"
+          class="m-1 d-none d-lg-inline px-3"
           variant="info"
           to="/about"
         >
           <b-icon size="lg" icon="info-circle"></b-icon>
         </b-button>
+      </div>
+      <div class="license d-flex align-items-center">
+        <a
+          rel="license"
+          href="http://creativecommons.org/licenses/by-nc-sa/4.0/"
+          ><img
+            alt="Creative Commons License"
+            style="border-width: 0"
+            src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png"
+        /></a>
+        <span class="d-none d-xl-inline">
+          This work is licensed under a
+          <a
+            rel="license"
+            href="http://creativecommons.org/licenses/by-nc-sa/4.0/"
+            >Creative Commons Attribution-NonCommercial-ShareAlike 4.0
+            International License</a
+          ></span
+        >
       </div>
     </div>
   </b-card>
@@ -94,5 +109,14 @@ export default defineComponent({
 
 .github-fork-ribbon:before {
   background-color: #333;
+}
+
+.license {
+  max-width: 37em;
+  font-size: 80%;
+  margin-right: 60px;
+}
+.license img {
+  margin-right: 3px;
 }
 </style>
