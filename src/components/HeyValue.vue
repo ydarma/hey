7ceb1bdc8e49322a632e1067c43ec546ad413b0d
Yves Darmaillac <ydarma@pm.me>
2022-02-21 04:26:50 +0100

    vector output fixed

--- src/components/HeyValue.vue
+++ src/components/HeyValue.vue
@@ -9,19 +9,23 @@
 
 <script lang="ts">
 import { Shape } from "@/libs/hey/shape";
+import { isVector } from "@/libs/hey/vector";
 import { defineComponent } from "vue";
 
 export default defineComponent({
   props: ["output"],
   computed: {
     isValue() {
-      return !this.isShape && typeof this.output != "object";
+      return !this.isShape;
     },
     isFunction() {
       return this.isValue && typeof this.output == "function";
     },
     isShape() {
       return this.output instanceof Shape;
+    },
+    isVector() {
+      return isVector(this.output);
     },
   },
 });
